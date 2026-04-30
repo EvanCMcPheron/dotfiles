a0ea874312f94d90bddb5bb846c7827208b0c177
evancm4 <evancm4@illinois.edu>
2026-04-30 15:30:26 -0500

    Added Idle Stuff

--- Utils/inhibit_idle.sh
+++ Utils/inhibit_idle.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
-echo "Inhibiting idling and handle-lid-switch until 'sleep infinity' terminates"
+# echo "Inhibiting idling and handle-lid-switch until 'sleep infinity' terminates"
+echo "Enabling Idle Inhibitor Vigiland"
 echo "Exit shell or terminate sleep with <Ctrl+C> to terminate systemd inhibition"
-sudo echo "Inhibition Enabled..."
-sudo systemd-inhibit --what="idle:handle-lid-switch" --why="User wants the screen to stay on!" sleep infinity
+echo "Inhibition Enabled..."
+# sudo systemd-inhibit --what="idle:handle-lid-switch" --why="User wants the screen to stay on!" sleep infinity
+vigiland
